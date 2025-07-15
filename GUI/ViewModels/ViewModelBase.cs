39ef8dad803c4543922d2889d77efeaf6e67e768
MoxLotus <brianblake1@live.com>
2025-07-14 19:48:53 -0600

    Helper Method to make the use of File Services easier.

--- GUI/ViewModels/ViewModelBase.cs
+++ GUI/ViewModels/ViewModelBase.cs
@@ -1,7 +1,15 @@
-﻿using ReactiveUI;
+﻿using GUI.Services;
+using ReactiveUI;
+using System;
 
 namespace GUI.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
+    protected IFileService GetFileAccessService()
+    {
+        IFileService? fileAccessService = App.Current?.Services?.GetService(typeof(IFileService)) as IFileService;
+        if (fileAccessService is null) throw new NullReferenceException("Missing File Service instance.");
+        return fileAccessService;
+    }
 }
