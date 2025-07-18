4a67adfdab806a28c74b06734c6b05e63d0c6621
MoxLotus <brianblake1@live.com>
2025-07-17 23:12:28 -0600

    Designing the backend workflow. Work in progress.

---
+++ Core/Builders/FFmpegArgumentComposer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonicate.Core.Builders;
+
+internal class FFmpegArgumentComposer
+{
+}
