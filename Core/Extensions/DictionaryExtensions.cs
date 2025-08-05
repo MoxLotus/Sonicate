4bb7188d79569863ad3392046aef0d0825e98e16
MoxLotus <brianblake1@live.com>
2025-08-05 00:33:33 -0600

    Added an extension method to string, string dictionaries to convert nulls to empty strings.

---
+++ Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonicate.Core.Extensions;
+
+public static class DictionaryExtensions
+{
+    /// <summary>
+    /// If the key exists in the dictionary, returns its value; otherwise, returns an empty string.
+    /// </summary>
+    public static string GetValueOrEmpty(this IDictionary<string, string> dictionary, string key)
+    {
+        return dictionary.TryGetValue(key, out string? value) ? value ?? string.Empty : string.Empty;
+    }
+}
