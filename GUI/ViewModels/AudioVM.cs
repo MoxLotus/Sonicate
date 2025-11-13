2c10e3c07d35751cfb22b69d1c1496ad77542bba
MoxLotus <brianblake1@live.com>
2025-11-12 20:27:36 -0700

    AudioView can now iterate through the files per audio track. Still needs a lot of bug hunting.

--- GUI/ViewModels/AudioVM.cs
+++ GUI/ViewModels/AudioVM.cs
@@ -1,6 +1,8 @@
 ﻿using Sonicate.Core.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +11,62 @@ namespace Sonicate.GUI.ViewModels;
 
 public class AudioVM : MainVM.Child
 {
+    public ObservableCollection<TrackInfoVM> Tracks { get; private set; } = [];
     public void AnalyzeAudioTracks(){
+        Tracks.Clear();
+
         // We grab the ViewModels so that we can select and deselect them in the UI.
-        List<List<TrackInfoVM>> matrix =
-            [.. (from file in Parent.FileSelect.MediaFiles
-             select
-                 (from track in file.Tracks
-                  where track.Track.Type == TrackInfo.TrackType.Audio
-                  select track
-                 ).ToList()
-            )];
+        List<List<AudioTrackInfoVM>> matrix =
+            [.. Parent.FileSelect.MediaFiles
+                .Select(file =>
+                    file.Tracks.OfType<AudioTrackInfoVM>()
+                    .ToList()
+                )
+            ];
 
-        int rows = 0;
-        foreach (var list in matrix)
-            if (list.Count > rows)
-                rows = list.Count;
+        int rows = matrix.Max(list => list.Count);
+        Debug.WriteLine($"AudioVM: Analyzing {matrix.Count} files with up to {rows} audio tracks.");
 
-        ;
+        for (int r = 0; r < rows; r++)
+        {
+            bool sameCodec = true;
+            bool sameChannels = true;
+            string codec = matrix[0][r].Track.Codec ?? "";
+            int channels = matrix[0][r].AudioTrack.Channels;
+            for (int c = 1; c < matrix.Count; c++)
+            {
+                if (matrix[c].Count <= r)
+                    break;
+                AudioTrackInfoVM candidate = matrix[c][r];
+                if (sameCodec)
+                {
+                    if (matrix[c].Count > r && !codec.Equals(candidate.Track.Codec))
+                    {
+                        sameCodec = false;
+                        codec = "varies";
+                    }
+                }
+
+                if (sameChannels)
+                {
+                    if (matrix[c].Count > r && channels != candidate.AudioTrack.Channels)
+                    {
+                        sameChannels = false;
+                        channels = -1;
+                    }
+                }
+
+                if (!sameCodec && !sameChannels)
+                    break;
+            }
+
+            Tracks.Add(new AudioTrackInfoVM(
+                new AudioTrackInfo()
+                {
+                    Codec = codec,
+                    Channels = channels,
+                }
+            ));
+        }
     }
 }
