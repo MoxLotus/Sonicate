03de11f758ff7f179e58d6502c874380e15103ed
MoxLotus <brianblake1@live.com>
2025-11-16 20:40:47 -0700

    Added another filter option in AudioView. Not fully implemented.

--- GUI/ViewModels/AudioVM.cs
+++ GUI/ViewModels/AudioVM.cs
@@ -1,9 +1,13 @@
-﻿using Sonicate.Core.DTOs;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using Sonicate.Core.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +15,9 @@ namespace Sonicate.GUI.ViewModels;
 
 public class AudioVM : MainVM.Child
 {
-    public ObservableCollection<AudioVMItem> Tracks { get; private set; } = [];
+    public ObservableCollection<AudioVMRow> Tracks { get; private set; } = [];
+    public ObservableCollection<LanguageFilterItem> AvailableLanguages { get; private set; } = [];
+
     public void AnalyzeAudioTracks(){
         Tracks.Clear();
 
@@ -27,5 +33,43 @@ public class AudioVM : MainVM.Child
         int rows = matrix.Max(list => list.Count);
         for (int r = 0; r < rows; r++)
             Tracks.Add(new([.. matrix.Where(c => r < c.Count).Select(c => c[r])]));
+
+        AvailableLanguages.Clear();
+        Tracks.SelectMany(t => t.Languages).Distinct().ToList().ForEach(i => AvailableLanguages.Add(new(i)));
+
+        foreach (var lang in AvailableLanguages)
+        {
+            lang.PropertyChanged += (s, e) =>
+            {
+                if (s is LanguageFilterItem source)
+                    onCheck(source);
+            };
+        }
+    }
+
+    public class LanguageFilterItem(string language) : INotifyPropertyChanged
+    {
+        public string Language { get; init; } = language;
+
+        private bool _selected = true;
+        public bool Selected
+        {
+            get => _selected;
+            set
+            {
+                if (value == _selected) return;
+                _selected = value;
+                OnPropertyChanged(nameof(Selected));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public void onCheck(LanguageFilterItem source)
+    {
+        Debug.WriteLine($"Language filter changed: {source.Language} -> {source.Selected}");
     }
 }
---
+++ GUI/ViewModels/AudioVMRow.cs
@@ -0,0 +1,49 @@
+﻿using ReactiveUI;
+using Sonicate.GUI.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonicate.GUI.ViewModels;
+
+public class AudioVMRow : INotifyPropertyChanged
+{
+    public List<AudioTrackInfoVM> AudioTracks { get; }
+    public List<string> Languages { get; }
+    public string Codec { get; }
+    public string Language { get; }
+    public int Channels { get; }
+    public string Layout { get; }
+    private bool _selected = true;
+
+    public bool Selected
+    {
+        get => _selected;
+        set
+        {
+            _selected = value;
+            AudioTracks.ForEach(t => t.Selected = value);
+            OnPropertyChanged(nameof(Selected));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    public AudioVMRow(List<AudioTrackInfoVM> tracks)
+    {
+        AudioTracks = tracks;
+        List<string> codecs = [.. tracks.Select(t => t.AudioTrack.Codec).Distinct()];
+        Languages = [.. tracks.Select(t => t.AudioTrack.Language).Distinct()];
+        List<int> channels = [.. tracks.Select(t => t.AudioTrack.Channels).Distinct()];
+        List<string> layouts = [.. tracks.Select(t => t.AudioTrack.Layout).Distinct()];
+        Codec = codecs.Count == 1 ? codecs[0] : "varies";
+        Language = Languages.Count == 1 ? Languages[0] : "varies";
+        Channels = channels.Count == 1 ? channels[0] : -1;
+        Layout = layouts.Count == 1 ? layouts[0] : "varies";
+    }
+}
